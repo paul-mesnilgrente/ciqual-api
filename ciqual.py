437e80f4ff65b9ea329dd6f7bf15f0fbf7f4c4b2
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-01-22 19:56:18 +1300

    add of the database

--- ciqual.py
+++ ciqual.py
@@ -1,1 +1,19 @@
-from app import app
+from app import app, db
+
+from app.models import Group, SubGroup, SubSubGroup
+from app.models import Food, Component, FoodComponent
+from app.models import Source, User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+    	'db': db,
+    	'User': User,
+    	'Group': Group,
+    	'SubGroup': Group,
+    	'SubSubGroup': Group,
+    	'Food': Food,
+    	'Component': Component,
+		'FoodComponent': FoodComponent,
+		'Source': Source,
+    }
